c5cc5505a1145d898fd317f78edccb6e925105c6
ganciaux <ghislain.anciaux@live.fr>
2025-02-06 22:34:06 +0100

    Backend: Add .env, logger, datasourse and utils

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,39 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { AppDataSource } from './data-source';
+import { logger } from './logger';
+import { getPort } from './utils';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = getPort(process.argv[2], process.env.PORT||'9001');
+  
+function setupExpress() {
+  app.use(cors());
+  app.use(bodyParser.json());
 
-app.use(cors());
-app.use(bodyParser.json());
+  app.get('/', (req, res) => {
+    res.send('Backend is running!');
+  });
+}
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+function startServer() {
+  app.listen(PORT, () => {
+    logger.info(
+      `HTTP REST API Server is npw running at http://localhost:${PORT}/`
+    );
 });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then((data) => {
+    logger.info("The datasource has bee initialized successfully.");
+    setupExpress();
+    startServer();
+  })
+  .catch((err) => {
+    logger.error("Can't initialized datasource.", err);
+    process.exit(1);
+  });
---
+++ backend/src/utils.ts
@@ -0,0 +1,25 @@
+import crypto from "crypto"
+import util from "util"
+
+const hashPassword = util.promisify(crypto.pbkdf2);
+
+const PORT = 9000;
+
+export function isInteger(input: string) {
+  return input?.match(/^\d+$/) ?? false;
+}
+
+export function getPort(arg: string, env: string) {
+  const defaultPort = PORT;
+
+  return isInteger(env)
+    ? parseInt(env)
+    : isInteger(arg)
+    ? parseInt(arg)
+    : defaultPort;
+}
+
+export async function calculatePasswordHash(plainTextPassword: string, passwordSalt: string) {
+    const passwordHash = await hashPassword(plainTextPassword, passwordSalt, 100000, 64, "sha512");
+    return passwordHash.toString("hex");
+}
